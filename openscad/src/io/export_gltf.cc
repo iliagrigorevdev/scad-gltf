83c5b60b06e393773ffdfbee8de14a507c6bc260
Ilia Grigorev <ru.igsoft@gmail.com>
2026-06-06 10:29:39 +0200

    Use default color and normal values for empty pixels

--- openscad/src/io/export_gltf.cc
+++ openscad/src/io/export_gltf.cc
@@ -630,9 +630,10 @@ void export_gltf(const std::shared_ptr<const Geometry>& geom, std::ostream& outp
                     }
                 }
 
-                auto dilate_and_encode = [&](std::vector<uint8_t>& px) -> std::string {
-                    for (int iter = 0; iter < 2; ++iter) {
+                auto dilate_and_encode = [&](std::vector<uint8_t>& px, uint8_t defR, uint8_t defG, uint8_t defB) -> std::string {
+                    for (int iter = 0; iter < 8; ++iter) {
                         std::vector<uint8_t> dilated_pixels = px;
+                        int changed = 0;
                         for (uint32_t y = 0; y < height; ++y) {
                             for (uint32_t x = 0; x < width; ++x) {
                                 int p_idx = (y * width + x) * 4;
@@ -660,11 +661,21 @@ void export_gltf(const std::shared_ptr<const Geometry>& geom, std::ostream& outp
                                         dilated_pixels[p_idx + 1] = g / count;
                                         dilated_pixels[p_idx + 2] = b / count;
                                         dilated_pixels[p_idx + 3] = 255;
+                                        changed++;
                                     }
                                 }
                             }
                         }
                         px = std::move(dilated_pixels);
+                        if (changed == 0) break;
+                    }
+                    for (size_t i = 0; i < px.size(); i += 4) {
+                        if (px[i + 3] == 0) {
+                            px[i + 0] = defR;
+                            px[i + 1] = defG;
+                            px[i + 2] = defB;
+                            px[i + 3] = 255;
+                        }
                     }
                     std::vector<unsigned char> png_data;
                     auto write_func = [](void *context, void *data, int size) {
@@ -675,8 +686,8 @@ void export_gltf(const std::shared_ptr<const Geometry>& geom, std::ostream& outp
                     return "data:image/png;base64," + base64_encode(png_data.data(), png_data.size());
                 };
 
-                if (has_colormap) mesh_base_color_uri = dilate_and_encode(pixels);
-                if (has_normalmap) mesh_normal_texture_uri = dilate_and_encode(npixels);
+                if (has_colormap) mesh_base_color_uri = dilate_and_encode(pixels, 128, 128, 128);
+                if (has_normalmap) mesh_normal_texture_uri = dilate_and_encode(npixels, 128, 128, 255);
             }
         }
 
